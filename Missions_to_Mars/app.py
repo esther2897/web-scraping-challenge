21c9a33c29472d9884f56b2831715151439f731b
Jessica Solomon <esther2897@gmail.com>
2021-09-12 21:58:50 -0400

    initial commit

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    # Find one record of data from the mongo database
+    mars = mongo.db.collection.find_one()
+    # Return template and data
+    return render_template("index.html", mars = mars)
+
+@app.route("/scrape")
+
+def scraper():
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
